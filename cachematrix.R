2faa9092ca996d20e2f289ac9fbb81b09842b07d
GerhardStimie <gstimie@gmail.com>
2018-10-26 19:02:50 +0200

    Update cachematrix.R
    First version of R course assignment 2 for submission for evaluation.

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,24 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function will take a square (assumed to be) non-singular matrix as input
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(input_mtx = matrix()) {
+        ## Initialize a local variable with nulls
+        local_mtx <- NULL
+        
+        ## 
+        
+        set <- function(free_mtx) {
+                local_mtx <<- free_mtx
+                local_mtx <<- NULL
+        }
+        get <- function() input_mtx
+        setinv <- function(solve) local_mtx <<- solve
+        getinv <- function() local_mtx
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
@@ -12,4 +26,16 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        
+        inv_mtx <- x$getinv()
+        
+        if (!is.null(inv_mtx)) {
+                message("You have inverted this matrix before. Getting cached matrix.")
+                return(inv_mtx)
+        }
+        message("This is the first time you are inverting this matrix. Using Solve function to calculate inverted matrix")
+        data <- x$get()
+        inv_mtx <- solve(data, ...)
+        x$setinv(inv_mtx)
+        inv_mtx
 }
